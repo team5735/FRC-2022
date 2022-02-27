5d731af078f1f47615fe72f627c69894bffc7bb8
geoffreywwang <geoffreywwang@gmail.com>
2022-02-27 14:09:02 -0500

    Change intake motor id

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -3,5 +3,5 @@ package frc.robot.constants;
 public class RobotConstants {
     public static final int DRIVE_CONTROLLER_PORT = 0;
 
-    public static final int INTAKE_MOTOR_ID = 0;
+    public static final int INTAKE_MOTOR_ID = 9;
 }
