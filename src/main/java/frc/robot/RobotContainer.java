9477a5b984f742947ffc5957ef18fac28a2a609d
jinglei <jinglei_whitehouse@intuit.com>
2022-02-27 15:56:55 -0500

    actual resolve conflicts

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,9 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.intake.IntakeIn;
 import frc.robot.commands.intake.IntakeOut;
@@ -31,36 +29,17 @@ public class RobotContainer {
   private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
   private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
 
-<<<<<<< HEAD
   public static final XboxController driveController = new XboxController(RobotConstants.DRIVE_CONTROLLER_PORT);
   public static final XboxController subsystemController = new XboxController(RobotConstants.SUBSYSTEM_CONTROLLER_PORT);
-=======
-  private final XboxController driveController = new XboxController(RobotConstants.DRIVE_CONTROLLER_PORT);
-  private final XboxController subsystemController = new XboxController(RobotConstants.SUBSYSTEM_CONTROLLER_PORT);
-
->>>>>>> e1a06f6d9ffd4a71142e8d2942e0a0502c636953
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
-<<<<<<< HEAD
-    configureDriveButtonBindings();
-  }
-
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureDriveButtonBindings() {
-=======
     configureDriveControllerBindings();
     configureSubsystemControllerBindings();
   }
 
   private void configureDriveControllerBindings() {
->>>>>>> e1a06f6d9ffd4a71142e8d2942e0a0502c636953
     new JoystickButton(driveController, Button.kRightBumper.value)
       .whenPressed(new IntakeIn(intakeSubsystem))
       .whenReleased(new IntakeStop(intakeSubsystem));
@@ -70,12 +49,10 @@ public class RobotContainer {
       .whenReleased(new IntakeStop(intakeSubsystem));
   }
 
-<<<<<<< HEAD
-=======
   private void configureSubsystemControllerBindings() {
     new JoystickButton(subsystemController, Button.kRightBumper.value)
       .whenPressed(new InstantCommand(shooterSubsystem::testShooter, shooterSubsystem))
       .whenReleased(new InstantCommand(shooterSubsystem::stopShooter, shooterSubsystem));
   }
->>>>>>> e1a06f6d9ffd4a71142e8d2942e0a0502c636953
+
 }
