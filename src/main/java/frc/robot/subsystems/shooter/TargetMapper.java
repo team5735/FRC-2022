48330bdb3b81ee4fa9319a9487a813a261050c56
jinglei <jinglei_whitehouse@intuit.com>
2022-03-22 21:31:41 -0400

    update angle and speed lookup

--- src/main/java/frc/robot/subsystems/shooter/TargetMapper.java
+++ src/main/java/frc/robot/subsystems/shooter/TargetMapper.java
@@ -9,28 +9,28 @@ import frc.robot.constants.LoggingConstants.LoggingLevel;
 public class TargetMapper {
     public static final Map<Integer, SpeedAngle> targetMapping = Map.ofEntries(
         //Key: distance by foot
-        Map.entry(3,new SpeedAngle(9000, 0.06, 1.0)),
-        Map.entry(4,new SpeedAngle(9100, 0.06, 1.0)),
-        Map.entry(5,new SpeedAngle(9200, 0.06, 1.0)),
-        Map.entry(6,new SpeedAngle(9300, 0.06, 1.0)),
-        Map.entry(7,new SpeedAngle(12750, 0.06, 1.0)),
-        Map.entry(8,new SpeedAngle(9500, 0.26, 1.2)),
-        Map.entry(9,new SpeedAngle(9600, 0.29, 1.5)),
-        Map.entry(10,new SpeedAngle(9700, 0.32, 1.5)),
-        Map.entry(11,new SpeedAngle(10200, 0.36, 1.5)),
-        Map.entry(12,new SpeedAngle(9900, 0.39, 1.5)),
-        Map.entry(13,new SpeedAngle(10000, 0.42, 1.5)),
-        Map.entry(14,new SpeedAngle(10100, 0.45, 1.5)),
-        Map.entry(15,new SpeedAngle(10200, 0.49, 1.5)),
-        Map.entry(16,new SpeedAngle(10300, 0.52, 1.5)),
-        Map.entry(17,new SpeedAngle(10400, 0.55, 1.5)),
-        Map.entry(18,new SpeedAngle(10500, 0.58, 1.5)),
-        Map.entry(19,new SpeedAngle(11000, 0.61, 1.5)),
-        Map.entry(20,new SpeedAngle(11000, 0.65, 1.5)),
-        Map.entry(21,new SpeedAngle(11900, 0.68, 1.5)),
-        Map.entry(22,new SpeedAngle(12000, 0.71, 1.5)),//ok
-        Map.entry(23,new SpeedAngle(12000, 0.74, 1.5)),
-        Map.entry(24,new SpeedAngle(15000, 0.75, 1.5))
+        Map.entry(3,new SpeedAngle(10000, 0.06, 1.5)),
+        Map.entry(4,new SpeedAngle(10000, 0.06, 1.5)),
+        Map.entry(5,new SpeedAngle(10000, 0.06, 1.5)),
+        Map.entry(6,new SpeedAngle(10000, 0.06, 1.5)),
+        Map.entry(7,new SpeedAngle(10000, 0.06, 1.5)),
+        Map.entry(8,new SpeedAngle(8728, 0.18, 1.5)),
+        Map.entry(9,new SpeedAngle(9096, 0.23, 1.5)),
+        Map.entry(10,new SpeedAngle(9438, 0.29, 1.5)),
+        Map.entry(11,new SpeedAngle(9758, 0.34, 1.5)),
+        Map.entry(12,new SpeedAngle(10060, 0.40, 1.5)),
+        Map.entry(13,new SpeedAngle(10346, 0.45, 1.5)),
+        Map.entry(14,new SpeedAngle(10618, 0.51, 1.5)),
+        Map.entry(15,new SpeedAngle(10878, 0.57, 1.5)),
+        Map.entry(16,new SpeedAngle(11126, 0.62, 1.5)),
+        Map.entry(17,new SpeedAngle(11365, 0.68, 1.5)),
+        Map.entry(18,new SpeedAngle(11595, 0.73, 1.5)),
+        Map.entry(19,new SpeedAngle(11817, 0.79, 1.5)),
+        Map.entry(20,new SpeedAngle(12031, 0.84, 1.5)),
+        Map.entry(21,new SpeedAngle(12238, 0.90, 1.5)),
+        Map.entry(22,new SpeedAngle(12439, 0.95, 1.5)),//ok
+        Map.entry(23,new SpeedAngle(12634, 0.95, 1.5)),
+        Map.entry(24,new SpeedAngle(12824, 0.95, 1.5))
     );
 
     public static SpeedAngle getSpeedAngleByDistance(double distance){
@@ -43,7 +43,7 @@ public class TargetMapper {
         if (distance > 288)
             return targetMapping.get(24);
 
-        double speed = 3595 * Math.pow(distance, 0.2494);
+        double speed = 1765 * Math.pow(distance, 0.3502);
 
         SpeedAngle floorSpeedAngle = targetMapping.get((int)Math.floor(distance/12));
         SpeedAngle ceilSpeedAngle = targetMapping.get((int)Math.floor((distance+12)/12));
@@ -56,11 +56,9 @@ public class TargetMapper {
             distance < 90? 1:1.5
         );
 
-
         SmartDashboard.putNumber("$Output Func", speed);
         SmartDashboard.putNumber("Output Speed", speedAngle.getSpeed());
         SmartDashboard.putNumber("Output Angle", speedAngle.getAngle());
-
 
         return speedAngle;
     }
