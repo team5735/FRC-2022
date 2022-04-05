14ba513030ad53c9a9556f0fd12646a0886652b8
jinglei <jinglei_whitehouse@intuit.com>
2022-04-04 22:36:04 -0400

    slow down intake for shooting

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -4,5 +4,5 @@ public class IntakeConstants {
     public static final double INTAKE_IN_SPEED = 0.6;
     public static final double INTAKE_OUT_SPEED = -0.6;
 
-    public static final double INTAKE_IN_FOR_SHOOTING_SPEED = 0.3;
+    public static final double INTAKE_IN_FOR_SHOOTING_SPEED = 0.2;
 }
