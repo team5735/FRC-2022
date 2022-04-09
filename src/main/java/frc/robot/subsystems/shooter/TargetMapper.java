81b14828cb513d496e24e80d65cf31a065020e58
btho7793 <77469772+btho7793@users.noreply.github.com>
2022-04-09 17:43:50 -0400

    shooting tuning constants

--- src/main/java/frc/robot/subsystems/shooter/TargetMapper.java
+++ src/main/java/frc/robot/subsystems/shooter/TargetMapper.java
@@ -14,39 +14,39 @@ public class TargetMapper {
         Map.entry(5,new SpeedAngle(10000, 0.06, 1.5)),
         Map.entry(6,new SpeedAngle(10000, 0.06, 1.5)),
         Map.entry(7,new SpeedAngle(10000, 0.06, 1.5)),
-        Map.entry(8,new SpeedAngle(8697, 0.18, 1.5)),
-        Map.entry(9,new SpeedAngle(9074, 0.23, 1.5)),
-        Map.entry(10,new SpeedAngle(9426, 0.29, 1.5)),
-        Map.entry(11,new SpeedAngle(9756, 0.34, 1.5)),
-        Map.entry(12,new SpeedAngle(10068, 0.40, 1.5)),
-        Map.entry(13,new SpeedAngle(10363, 0.45, 1.5)),
-        Map.entry(14,new SpeedAngle(10644, 0.51, 1.5)),
-        Map.entry(15,new SpeedAngle(10912, 0.57, 1.5)),
-        Map.entry(16,new SpeedAngle(11169, 0.62, 1.5)),
-        Map.entry(17,new SpeedAngle(11416, 0.68, 1.5)),
-        Map.entry(18,new SpeedAngle(11654, 0.73, 1.5)),
-        Map.entry(19,new SpeedAngle(11884, 0.79, 1.5)),
-        Map.entry(20,new SpeedAngle(12106, 0.84, 1.5)),
-        Map.entry(21,new SpeedAngle(12321, 0.90, 1.5)),
-        Map.entry(22,new SpeedAngle(12530, 0.95, 1.5)),//ok
-        Map.entry(23,new SpeedAngle(13252, 0.95, 1.5)),
-        Map.entry(24,new SpeedAngle(13850, 0.95, 1.5)),
-        Map.entry(25,new SpeedAngle(14449, 0.95, 1.5)),
-        Map.entry(26,new SpeedAngle(15047, 0.95, 1.5))
+        Map.entry(8,new SpeedAngle(9004, 0.2, 1.5)),
+        Map.entry(9,new SpeedAngle(9400, 0.28, 1.5)),
+        Map.entry(10,new SpeedAngle(9769, 0.37, 1.5)),
+        Map.entry(11,new SpeedAngle(10116, 0.45, 1.5)),
+        Map.entry(12,new SpeedAngle(10443, 0.53, 1.5)),
+        Map.entry(13,new SpeedAngle(10753, 0.61, 1.5)),
+        Map.entry(14,new SpeedAngle(11048, 0.70, 1.5)),
+        Map.entry(15,new SpeedAngle(11331, 0.78, 1.5)),
+        Map.entry(16,new SpeedAngle(11601, 0.86, 1.5)),
+        Map.entry(17,new SpeedAngle(11861, 0.93, 1.5)),
+        Map.entry(18,new SpeedAngle(12112, 0.93, 1.5)),
+        Map.entry(19,new SpeedAngle(12500, 0.93, 1.5)),
+        Map.entry(20,new SpeedAngle(12916, 0.93, 1.5)),
+        Map.entry(21,new SpeedAngle(13333, 0.93, 1.5)),
+        Map.entry(22,new SpeedAngle(13750, 0.93, 1.5)),//ok
+        Map.entry(23,new SpeedAngle(14166, 0.93, 1.5)),
+        Map.entry(24,new SpeedAngle(14583, 0.93, 1.5)),
+        Map.entry(25,new SpeedAngle(15000, 0.93, 1.5)),
+        Map.entry(26,new SpeedAngle(15416, 0.93, 1.5))
     );
 
     public static SpeedAngle getSpeedAngleByDistance(double distance){
 
         if (distance < 90)
             return targetMapping.get(7);
-        if (distance > 264) {
-            double speed = 49.875 * distance - 513.74;
-            SpeedAngle speedAngle = new SpeedAngle((int)speed, 0.95, 1.5);
+        if (distance > 216) {
+            double speed = 34.722 * distance + 4583;
+            SpeedAngle speedAngle = new SpeedAngle((int)speed, 0.93, 1.5);
             return speedAngle;
             //return targetMapping.get(24);
         }
 
-        double speed = 1487 * Math.pow(distance, 0.3856);
+        double speed = 1696 * Math.pow(distance, 0.3657);
 
         SpeedAngle floorSpeedAngle = targetMapping.get((int)Math.floor(distance/12));
         SpeedAngle ceilSpeedAngle = targetMapping.get((int)Math.floor((distance+12)/12));
